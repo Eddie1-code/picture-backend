7ae41f646a9a472c8eb3c59fd46d1fc96d3e3236
许灿炜 <xcw2351@smail.seig.edu.cn>
2025-05-25 18:54:51 +0800

    后端初始化完成

---
+++ src/main/java/com/xcw/picturebackend/PictureBackendApplication.java
@@ -0,0 +1,15 @@
+package com.xcw.picturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.xcw.picturebackend.mapper")
+@EnableAspectJAutoProxy(proxyTargetClass = true)//默认使用jdk动态代理，设置为true使用cglib动态代理
+public class PictureBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackendApplication.class, args);
+    }
+}
---
+++ src/main/java/com/xcw/picturebackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.xcw.picturebackend.exception;
+
+import com.xcw.picturebackend.common.BaseResponse;
+import com.xcw.picturebackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
